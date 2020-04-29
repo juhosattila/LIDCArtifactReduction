c392bbcead4616064ce407a5b0ed6df1f17a05bc
Attila Juhos <juhosattila6@gmail.com>
2020-04-29 20:12:19 +0300

    Test set up.

--- preprocess.py
+++ preprocess.py
@@ -19,7 +19,26 @@ def run():
             np.savez(file=file, rec=rec, sino=sino)
 
 
+def test():
+    patient_list = [1, 3, 5, 14]
+    patient_ids = ["LIDC-IDRI-" + "{:04d}".format(id) for id in patient_list]
+
+    dl = DicomLoader(batch_size=2).filter(patient_ids)
+
+    radon_trans = ParallelRadonTransform(img_side_length=parameters.IMG_SIDE_LENGTH,
+                    angles=np.linspace(0.0, 180.0, parameters.NR_OF_SPARSE_ANGLES))
+    i = 0
+    for data_batch in dl:
+        data_batch_resized, data_batch_sino = run_transformations(data_batch[:20], radon_trans)
+        for rec, sino in zip(data_batch_resized, data_batch_sino):
+            filename = '{:05}'.format(i)
+            i += 1
+            file = os.path.join(parameters.DATA_DIRECTORY, filename)
+            np.savez(file=file, rec=rec, sino=sino)
+
+
 if __name__ == "__main__":
-    run()
+    #run()
+    test()
 
 
