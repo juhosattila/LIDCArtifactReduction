8cdf7d4606a2bfe83023898d450e3aa42154bd91
Attila Juhos <juhosattila6@gmail.com>
2020-05-11 01:20:32 +0300

    Test preprocess added.

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 from LIDCArtifactReduction import parameters
+from LIDCArtifactReduction.array_streams import RecSinoArrayStream
 from LIDCArtifactReduction.dicom_preprocess import DicomLoader
-
 from LIDCArtifactReduction.radon_params import RadonParams
 from LIDCArtifactReduction.offline_transformation import ResizeRescaleRadonOfflineTransformation
+
 
 dl = DicomLoader(batch_size=10, ignored_edge_slice=0.1)
 img_side_length = parameters.IMG_SIDE_LENGTH
 radon_params = RadonParams(angles=np.linspace(0.0, 180.0, parameters.NR_OF_SPARSE_ANGLES))
 offline_transformation = ResizeRescaleRadonOfflineTransformation(img_side_length, radon_params)
 dl.run_offline_transformations(offline_transformation)
+
+
