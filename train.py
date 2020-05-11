af4e66ded1d4c924be4487b11c8ad5d23a686a9c
Attila Juhos <juhosattila6@gmail.com>
2020-05-11 19:16:31 +0300

    Correction 3.

--- train.py
+++ train.py
@@ -16,8 +16,8 @@ training_network = DCAR_TrainingNetwork(radon_params, target_model=unet,
                                         name='DCAR_UNet_FewBatchNorms_Training')
 training_network.set_training(training=True)
 training_network.compile(
-    adam_lr=1e-2, sino_output_weight=1.0 / parameters.NR_OF_SPARSE_ANGLES,
-    total_variation_eps=1.0, tot_var_loss_weight=5 * 1e-3)
+    adam_lr=1e-3, sino_output_weight=1.0 / parameters.NR_OF_SPARSE_ANGLES,
+    total_variation_eps=1.0, tot_var_loss_weight=1e-3)
 
 generator = LIDCDataGenerator(verbose=True, validation_split=0.25, test_split=0.25, batch_size=10)
 training_network.fit(generator.train_iterator, generator.valid_iterator, epochs=5)
