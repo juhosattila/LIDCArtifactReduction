d699c0a607d7abc70e24fc83e29c7da8101723fd
Attila Juhos <juhosattila6@gmail.com>
2020-05-11 12:26:55 +0300

    TF Addon version check 2.

--- LIDCArtifactReduction/__init__.py
+++ LIDCArtifactReduction/__init__.py
@@ -19,5 +19,6 @@ if platform.system() == 'Linux':
 
 import tensorflow_addons as tfa
 # Register in order to be able to save and load Radon layers.
-if tfa.__version__[1] >= 9:
+from packaging import version
+if version.parse(tfa.__version__) >= version.parse("0.9.1"):
     tfa.register_all()
