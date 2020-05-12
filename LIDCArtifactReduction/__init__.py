49d89a5f92cb3bedb7d83b172ea1006ec3f5c036
Attila Juhos <juhosattila6@gmail.com>
2020-05-13 01:25:33 +0300

    Memory limit setter function created.

--- LIDCArtifactReduction/__init__.py
+++ LIDCArtifactReduction/__init__.py
@@ -1,21 +1,27 @@
 import sys
 import tensorflow as tf
 import platform
-# Purely dependent of server nhakni.
-if platform.system() == 'Linux':
-    # Add my package folder, if necessary.
-    # sys.path.insert(1, "/home/juhosa/python-packages")
-    # print(sys.path)
 
-    # Set resource limit of tensorflow on server
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    try:
-        tf.config.experimental.set_virtual_device_configuration(gpus[0],
-        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=11000)])
-        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-    except RuntimeError as e:
-        print(e)
 
+def set_memory_limit(memory_limit=None):
+    if memory_limit is None:
+        memory_limit = 6000
+
+    # Purely dependent of server nhakni.
+    if platform.system() == 'Linux':
+        # Add my package folder, if necessary.
+        # sys.path.insert(1, "/home/juhosa/python-packages")
+        # print(sys.path)
+
+        # Set resource limit of tensorflow on server
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        try:
+            tf.config.experimental.set_virtual_device_configuration(gpus[0],
+                                                                    [tf.config.experimental.VirtualDeviceConfiguration(
+                                                                        memory_limit=11000)])
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        except RuntimeError as e:
+            print(e)
 
 import tensorflow_addons as tfa
 # Register in order to be able to save and load Radon layers.
