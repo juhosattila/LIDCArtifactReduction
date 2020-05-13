08feba31b8b650eb34e065a42ceab3a5cc682aa5
Attila Juhos <juhosattila6@gmail.com>
2020-05-13 23:41:09 +0300

    Memory limit setting repaired 2.

--- LIDCArtifactReduction/__init__.py
+++ LIDCArtifactReduction/__init__.py
@@ -1,11 +1,8 @@
-def init(memory_limit=None):
+def init(gpu_memory_limit_MB=None):
     import tensorflow as tf
     import platform
 
-    def set_memory_limit(memory_limit=None):
-        if memory_limit is None:
-            memory_limit = 6500
-
+    def set_memory_limit(gpu_mem_limit):
         # Purely dependent of server nhakni.
         if platform.system() == 'Linux':
             # Add my package folder, if necessary.
@@ -16,13 +13,13 @@ def init(memory_limit=None):
             gpus = tf.config.experimental.list_physical_devices('GPU')
             try:
                 tf.config.experimental.set_virtual_device_configuration(gpus[0],
-                                                                        [tf.config.experimental.VirtualDeviceConfiguration(
-                                                                            memory_limit=memory_limit)])
+                    [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=gpu_mem_limit)])
                 logical_gpus = tf.config.experimental.list_logical_devices('GPU')
             except RuntimeError as e:
                 print(e)
 
-    set_memory_limit(memory_limit=memory_limit)
+    if gpu_memory_limit_MB is not None:
+        set_memory_limit(gpu_mem_limit=gpu_memory_limit_MB)
 
     import tensorflow_addons as tfa
     # Register in order to be able to save and load Radon layers.
