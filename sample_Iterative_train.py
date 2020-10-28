fc9a58a637b361b4749eabf65e6dfe752bdd15ad
Juhos Attila <juhosattila6@gmail.com>
2020-10-28 03:06:40 +0100

    Started corrections.

--- sample_Iterative_train.py
+++ sample_Iterative_train.py
@@ -1,22 +1,17 @@
 # Needed in this sequence
 import LIDCArtifactReduction
-#LIDCArtifactReduction.init()
+LIDCArtifactReduction.init()
+#LIDCArtifactReduction.init(gpu_memory_limit_MB=6700)
+
+from LIDCArtifactReduction.radon_transformation.radon_geometry import RadonGeometry
+from LIDCArtifactReduction.array_streams import RecSinoArrayStream
+from LIDCArtifactReduction.directory_system import DirectorySystem
+from LIDCArtifactReduction.generator.generator import LIDCDataGenerator
 from LIDCArtifactReduction.neural_nets.iterative_ART_ResNet.IterativeARTResNet import IterativeARTResNet
 from LIDCArtifactReduction.neural_nets.iterative_ART_ResNet.IterativeARTResNetTraining import IterativeARTResNetTraining
 from LIDCArtifactReduction.neural_nets.iterative_ART_ResNet.IterativeARTResNet_generator_transform import \
     IterativeARTResNetGeneratorTransform
 from LIDCArtifactReduction.radon_transformation.radon_transformation_pyronn import PyronnParallelARTRadonTransform
-
-LIDCArtifactReduction.init(gpu_memory_limit_MB=6700)
-
-from LIDCArtifactReduction.radon_transformation.radon_geometry import RadonGeometry
-from LIDCArtifactReduction.radon_transformation.radon_transformation_orig import ParallelRadonTransform
-from LIDCArtifactReduction.neural_nets.FBPPostProcessingResNet.target_networks import DCAR_UNet_FewBatchNorms
-from LIDCArtifactReduction.neural_nets.FBPPostProcessingResNet.DCAR_TrainingNetwork import DCAR_TrainingNetwork
-from LIDCArtifactReduction.array_streams import RecSinoArrayStream
-from LIDCArtifactReduction.directory_system import DirectorySystem
-from LIDCArtifactReduction.generator.generator import LIDCDataGenerator
-from LIDCArtifactReduction.neural_nets.FBPPostProcessingResNet.FBPConvnet_generator_transform import FBPConvnetGeneratorTransform
 
 
 geometry = RadonGeometry(volume_img_width=256, projection_width=256, nr_projections=40)
@@ -26,7 +21,7 @@ ds = DirectorySystem(geometry, data_name='pyronn', algorithm_name='iter')
 array_stream = RecSinoArrayStream(directory=ds.DATA_DIRECTORY)  # or ds.SAMPLE_DATA_DIRECTORY
 
 generator = LIDCDataGenerator(array_stream=array_stream, load_data_config='data_config_file', verbose=True,
-                            validation_split=0.1, test_split=0.1, batch_size=8)
+                            validation_split=0.1, test_split=0.1, batch_size=16)
 # For noisy transformations you may set lnI0 and sumscaling.
 noisy_transformer = IterativeARTResNetGeneratorTransform(geometry, radon_transform, add_noise=True
                                                          )#mode=4, #, lnI0=10*np.log(4), sum_scaling=5.0)
@@ -49,4 +44,4 @@ training_network.compile(
     reconstructions_output_weight=1.0,
     error_singrom_weight=1.0 / (1.0 * geometry.volume_img_width),
     gradient_weight=1.0)
-training_network.train(train_iterator, final_depth=5, steps_per_epoch=400)
+training_network.train(train_iterator, final_depth=5, steps_per_epoch=100)
