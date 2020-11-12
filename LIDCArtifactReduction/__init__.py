6d3b32470306bf177ec434148277b0e8017f6676
Juhos Attila <juhosattila6@gmail.com>
2020-11-13 00:59:19 +0100

    Changed GPU initialization to work with multiple GPU-s.

--- LIDCArtifactReduction/__init__.py
+++ LIDCArtifactReduction/__init__.py
@@ -1,41 +1,40 @@
-def init(gpu_memory_limit_MB=None):
-    """Initialising the package is mandatory. Initialises GPU memory and the Tensorflow Addons library.
+def init(gpu_id=0, gpu_memory_limit_MB=None):
+    """Initialising the package is mandatory. Initialises GPU id, GPU memory and the Tensorflow Addons library.
 
     The Tensorflow Addons library must be initialised in order to be able to save and load Radon layers.
 
     Args:
+        gpu_id: should be id of GPU in the list produced by tf.config.experimental.list_physical_devices('GPU').
+            NOW:
+                0 -- Titan Xp
+                1 -- GTX 1080 Ti
         gpu_memory_limit_MB: could be None, in which case the system allocates the amount of memory necessary.
     """
     import tensorflow as tf
     import platform
 
-    def set_memory_limit(gpu_mem_limit):
-        # Purely dependent of server nhakni.
-        if platform.system() == 'Linux':
-            # Add my package folder, if necessary.
-            # sys.path.insert(1, "/home/juhosa/python-packages")
-            # print(sys.path)
+    if platform.system() == 'Linux':
+        # Add my package folder, if necessary.
+        # sys.path.insert(1, "/home/juhosa/python-packages")
+        # print(sys.path)
 
-            # Set resource limit of tensorflow on server
-            gpus = tf.config.experimental.list_physical_devices('GPU')
-            try:
-                # Only set for gpus[0].
-                tf.config.experimental.set_virtual_device_configuration(gpus[0],
-                    [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=gpu_mem_limit)])
-                print("Tensorflow: memory limit is set to {}".format(gpu_mem_limit))
+        physical_gpus = tf.config.experimental.list_physical_devices('GPU')
+        used_physical_gpu = physical_gpus[gpu_id]
+        tf.config.set_visible_devices([used_physical_gpu], 'GPU')
+        try:
+            if gpu_memory_limit_MB is not None:
 
-                # # TODO: somwhow solve this issue with PyPi pyronn package.
-                # tf.config.experimental.set_memory_growth(gpus[0], True)
-                # print("Tensorflow: memory_growth is set to True")
+                tf.config.experimental.set_virtual_device_configuration(used_physical_gpu,
+                    [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=gpu_memory_limit_MB)])
+                print("Tensorflow: memory limit is set to {}".format(gpu_memory_limit_MB))
 
-                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            except RuntimeError as e:
-                print(e)
+            # # TODO: somwhow solve this issue with PyPi pyronn package.
+            # tf.config.experimental.set_memory_growth(gpus[0], True)
+            # print("Tensorflow: memory_growth is set to True")
 
-
-    # Set GPU memory limit if necessary
-    if gpu_memory_limit_MB is not None:
-        set_memory_limit(gpu_mem_limit=gpu_memory_limit_MB)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        except RuntimeError as e:
+            print(e)
 
 
     # Register in order to be able to save and load Radon layers.
